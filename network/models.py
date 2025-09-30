3c23e3303b6de6c102b8e11ed90a2d60188d930c
Rafael Mourato <rafaelmourato@MacBook-Air-de-Rafael-2.local>
2025-09-29 23:43:55 -0300

    Making models for the code

--- network/models.py
+++ network/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    content = models.TextField(max_length=256)
+    creator = models.ForeignKey("User", on_delete=models.CASCADE, related_name="creator")
+    likes = models.ManyToManyField("User", related_name="likes")
+    time = models.DateTimeField(auto_now_add=True)
+
+class Follow(models.Model):
+    follower = models.ForeignKey("User", on_delete=models.CASCADE, related_name="following")
+    followed = models.ForeignKey("User", on_delete=models.CASCADE, related_name="followers")
