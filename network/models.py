c03cbda98f961e968be04d42c6b1cba8a9634e92
Rafael Mourato <rafaelmourato@MacBook-Air-de-Rafael-2.local>
2025-10-20 21:47:48 -0300

    returning the project

--- network/models.py
+++ network/models.py
@@ -8,9 +8,12 @@ class User(AbstractUser):
 class Post(models.Model):
     content = models.TextField(max_length=256)
     creator = models.ForeignKey("User", on_delete=models.CASCADE, related_name="creator")
-    likes = models.ManyToManyField("User", related_name="likes")
+    likes = models.ManyToManyField("User", related_name="likes", blank=True, null=True)
     time = models.DateTimeField(auto_now_add=True)
 
 class Follow(models.Model):
     follower = models.ForeignKey("User", on_delete=models.CASCADE, related_name="following")
     followed = models.ForeignKey("User", on_delete=models.CASCADE, related_name="followers")
+    
+    class Meta:
+        unique_together = ("follower", "followed")
--- network/views.py
+++ network/views.py
@@ -1,15 +1,44 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post, Follow
 
 
 def index(request):
-    return render(request, "network/index.html")
+    if request.method == 'POST':
+        if request.user.is_authenticated :
+            content_data = request.POST
+            post = Post(
+                creator = request.user,
+                content = content_data,
+            )
+            post.save()
+            return HttpResponseRedirect(reverse("index"))
+    posts = Post.objects.all().order_by("-time")
+    return render(request, "network/index.html",  {
+        "posts": posts
+    })
 
+def profile(request, creator):
+    creatordata = User.objects.get(username=creator)
+    follow = Follow.objects.all().filter(follower=creatordata)
+    followedby = Follow.objects.all().filter(followed=creatordata)
+    posts = Post.objects.all().filter(creator=creatordata).order_by("-time")
+    return render(request, "network/profile.html",  {
+        "following": follow,
+        "followers": followedby,
+        "posts": posts
+    })
+    
+@login_required
+def following(request):
+    following = User.objects.filter(followers__follower = request.user)
+    posts = Post.objects.filter(creator__in=following).order_by("-time")
+    pass
 
 def login_view(request):
     if request.method == "POST":
